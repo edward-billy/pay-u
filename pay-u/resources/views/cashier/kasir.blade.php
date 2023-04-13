54f9b95b4adc4f2e0101bc791658fda5b27d9055
fadhilfadh <fadhilfadhlu@student.telkomuniversity.ac.id>
2023-04-13 22:52:40 +0700

    Update kasir.blade.php

--- pay-u/resources/views/cashier/kasir.blade.php
+++ pay-u/resources/views/cashier/kasir.blade.php
@@ -19,10 +19,13 @@
         <a href="{{ url('/cashier/kategori/') }}?category=1" id="addstok" style="z-index: 1">Makanan</a>
         <a href="{{ url('/cashier/kategori/') }}?category=2" id="addstok" style="z-index: 1">Minuman</a>
         <br><br><a href="{{ url('/cashier/kategori/') }}?category=3" id="addstok" style="z-index: 1">Cemilan</a>
-        <a href="{{ url('/cashier/kategori/') }}?category=4" id="addstok" style="z-index: 1">Elektronik</a>
-        <br><br><a href="{{ url('/cashier/kategori/') }}?category=5" id="addstok" style="z-index: 1">Furniture</a>
-        <a href="{{ url('/cashier/kategori/') }}?category=6" id="addstok" style="z-index: 1">Kecantikan</a>
-        <br><br><a href="{{ url('/cashier/kategori/') }}?category=7" id="addstok" style="z-index: 1">Kesehatan</a>
+        <a href="{{ url('/cashier/kategori/') }}?category=4" id="addstok" style="z-index: 1">Pakaian</a>
+        <br><br><a href="{{ url('/cashier/kategori/') }}?category=5" id="addstok" style="z-index: 1">Elektronik</a>
+        <a href="{{ url('/cashier/kategori/') }}?category=6" id="addstok" style="z-index: 1">Furnitur</a>
+        <br><br><a href="{{ url('/cashier/kategori/') }}?category=7" id="addstok" style="z-index: 1">Kecantikan</a>
+        <a href="{{ url('/cashier/kategori/') }}?category=8" id="addstok" style="z-index: 1">Olahraga</a>
+        <br><br><a href="{{ url('/cashier/kategori/') }}?category=9" id="addstok" style="z-index: 1">Kesehatan</a>
+        <a href="{{ url('/cashier/kategori/') }}?category=10" id="addstok" style="z-index: 1">Hobi</a>
 
 
     </body>
